55f1e0dcb82d0da11acb304b22629a7b033e6c21
Christopher O Fallah <your-email@example.com>
2025-09-20 05:10:46 +0100

    New Page Updates

--- app/services/annitapay-business/page.tsx
+++ app/services/annitapay-business/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from 'react'
+import { useState, memo } from 'react'
 import { motion } from 'framer-motion'
 import { useInView } from 'framer-motion'
 import { useRef } from 'react'
