e8a2443ccd90244b3ab6ac106c1096080ab21cda
Christopher O Fallah <your-email@example.com>
2025-12-16 12:22:23 +0000

    Deployment

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -18,11 +18,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly' as const,
       priority: 0.9,
     },
-    {
-      lastModified: currentDate,
-      changeFrequency: 'weekly' as const,
-      priority: 0.9,
-    },
       {
         url: `${baseUrl}/contact`,
         lastModified: currentDate,
